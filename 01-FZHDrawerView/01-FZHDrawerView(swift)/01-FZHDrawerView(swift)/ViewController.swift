f62ee4da9f96ab38ae21744c2acf4cb3a6114cef
fengzhihao123 <1374795493@qq.com>
2016-10-13 11:32:10 +0800

    添加leftItem

--- 01-FZHDrawerView/01-FZHDrawerView(swift)/01-FZHDrawerView(swift)/ViewController.swift
+++ 01-FZHDrawerView/01-FZHDrawerView(swift)/01-FZHDrawerView(swift)/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLeftItem()
         view.backgroundColor = UIColor.red
     }
 
+    func setupLeftItem() -> Void {
+        let leftItem = UIBarButtonItem.init(title: "left", style: .done, target: self, action: #selector(itemDidTouch))
+        navigationItem.leftBarButtonItem = leftItem
+    }
     
+    func itemDidTouch() -> Void {
+        let temAppDelegate = UIApplication.shared.delegate as! AppDelegate
+        if temAppDelegate.fzhDrawerVC.isShow {
+            temAppDelegate.fzhDrawerVC.hideLeftView()
+        }else{
+            temAppDelegate.fzhDrawerVC.showLeftView()
+        }
+    }
 }
 
