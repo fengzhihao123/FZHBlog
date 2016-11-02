00a4731459a69342be10d320f1ebfa05f3916af2
fengzhihao123 <1374795493@qq.com>
2016-11-02 17:29:57 +0800

    add 06

---
+++ 06-FZHBlurred/06-FZHBlurredTests/_6_FZHBlurredTests.swift
@@ -0,0 +1,36 @@
+//
+//  _6_FZHBlurredTests.swift
+//  06-FZHBlurredTests
+//
+//  Created by 冯志浩 on 16/11/2.
+//  Copyright © 2016年 FZH. All rights reserved.
+//
+
+import XCTest
+@testable import _6_FZHBlurred
+
+class _6_FZHBlurredTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
